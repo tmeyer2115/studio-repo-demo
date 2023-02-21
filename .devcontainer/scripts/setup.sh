d8df109ec7ca4ee07cd1743d1eb6a4e5bf8e3872
Tom Meyer <tmeyer@yext.com>
2023-02-21 11:16:13 -0500

    Update setup.sh

--- .devcontainer/scripts/setup.sh
+++ .devcontainer/scripts/setup.sh
@@ -2,6 +2,7 @@
 source $NVM_DIR/nvm.sh
 nvm install 17
 nvm use 17
+cd /workspaces/studio-repo-demo
 npm install
 wget https://yext-cli-pub.s3.amazonaws.com/cli/linux/yext
 sudo mv yext /usr/local/bin
